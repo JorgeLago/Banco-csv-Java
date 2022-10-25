5e442eb6a19b202a10f4e9b3083f5e66ecd90a36
Jorge Lago <jorgelago92@gmail.com>
2022-10-25 00:51:13 -0300

    tudo

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,3 +1,16 @@
 public class CriaConta {
-    
+    public static void main(String[] args) {
+        Cliente jorge = new Cliente();
+        jorge.nome = " Jorge Lago";
+        jorge.cpf = "334.334.223.99";
+        jorge.profissao = "programador";
+        
+        Conta contaDoJorge = new Conta();
+        contaDoJorge.agencia = 2332;
+        contaDoJorge.numero = 422;
+        contaDoJorge.saldo = 7000;
+        contaDoJorge.titular = jorge;
+
+        System.out.println(contaDoJorge.saldo);
+    }
 }
